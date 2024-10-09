80f2dff7e355e081de1669ce2e37f052f4f368f7
Renee D'Netto <rdnetto@gmail.com>
2024-10-09 04:59:31 -0700

    Added get_tickers()

--- src/exchanges.rs
+++ src/exchanges.rs
@@ -1,5 +1,6 @@
 use log::warn;
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DefaultOnError};
 
 use super::{env_eodhd_token, EODHDError};
 
@@ -44,6 +45,89 @@ pub async fn get_exchanges() -> Result<Vec<EODHDExchange>, EODHDError> {
     let res = request.unwrap().json::<Vec<EODHDExchange>>().await;
     if res.is_err() {
         let description: &str = "parsing exchanges failed";
+        warn!("{}", description);
+        return Err(EODHDError {
+            description: description.to_string(),
+            inner_error: Box::new(res.err().unwrap()),
+        });
+    }
+    Ok(res.unwrap())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum EODHDTickerType {
+    #[serde(alias = "Common Stock")]
+    CommonStock,
+    #[serde(alias = "Preferred Stock")]
+    PreferredStock,
+    // Both common + preferred
+    Stock,
+    ETF,
+    Fund
+}
+
+#[serde_as]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct EODHDExchangeTicker {
+    // Ticker code. e.g. CBA
+    pub code: String,
+    // Ticker name. e.g. Commonwealth Bank Of Austrlia
+    pub name: String,
+    // Full country name
+    pub country: String,
+    // Exchange code
+    pub exchange: String,
+    // Exchange currency
+    pub currency: String,
+    // Ticker type. None if of unknown type.
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    #[serde(rename = "Type")]
+    pub ticker_type: Option<EODHDTickerType>,
+    // ISIN - uniquely identifies ticker across exchanges.
+    pub isin: Option<String>,
+}
+
+pub async fn get_tickers(
+    exchange: &str,
+    // If specified, only return tickers of the given type.
+    ticker_type: Option<EODHDTickerType>
+    ) -> Result<Vec<EODHDExchangeTicker>, EODHDError> {
+    let mut url = format!(
+        "{base_url}/exchange-symbol-list/{exchange}/?api_token={token}&fmt=json",
+        base_url = BASE_URL,
+        exchange = exchange,
+        token = env_eodhd_token()
+    );
+
+    if ticker_type.is_some() {
+        url = format!(
+            "{url}&type={typ}",
+            url = url,
+            typ = match ticker_type.unwrap() {
+                EODHDTickerType::CommonStock => "common_stock",
+                EODHDTickerType::PreferredStock => "preferred_stock",
+                EODHDTickerType::Stock => "stock",
+                EODHDTickerType::ETF => "etf",
+                EODHDTickerType::Fund => "fund"
+            }
+        );
+    }
+
+    let request = reqwest::get(url).await;
+
+    if request.is_err() {
+        let description: &str = "request failed";
+        warn!("{}", description);
+        return Err(EODHDError {
+            description: description.to_string(),
+            inner_error: Box::new(request.err().unwrap()),
+        });
+    }
+
+    let res = request.unwrap().json::<Vec<EODHDExchangeTicker>>().await;
+    if res.is_err() {
+        let description: &str = "parsing tickers failed";
         warn!("{}", description);
         return Err(EODHDError {
             description: description.to_string(),
